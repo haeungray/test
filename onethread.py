0eea2c342434beef4f6474a6c6e0bdbb4790c5d5
haeungray <haeungray@gmail.com>
2020-05-15 00:33:31 +0900

    python thread test

---
+++ onethread.py
@@ -0,0 +1,29 @@
+from time import sleep, time, ctime
+
+def loop0():
+    print("start loop 0 at : ", ctime(time()))
+    sleep(4)
+    print("loop 0 done at : ", ctime(time()))
+
+def loop1():
+    print('start loop 1 at : ', ctime(time()))
+    sleep(2)
+    print("loop 1 done at : ", ctime(time()))
+
+def main():
+    print("starting ...")
+    loop0()
+    loop1()
+    print("All Done at :", ctime(time()))
+
+
+'''
+starting ...
+start loop 0 at :  Fri May 15 00:32:41 2020
+loop 0 done at :  Fri May 15 00:32:45 2020
+start loop 1 at :  Fri May 15 00:32:45 2020
+loop 1 done at :  Fri May 15 00:32:47 2020
+All Done at : Fri May 15 00:32:47 2020
+'''
+if __name__ == "__main__":
+    main()
