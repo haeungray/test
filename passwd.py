d5283a0471c3617af6f3903a4be99d78febc242c
haeungray <haeungray@gmail.com>
2020-05-21 23:31:34 +0900

    function for _ in range(n)

--- passwd.py
+++ passwd.py
@@ -5,5 +5,6 @@ _LENGTH = 10
 i = randint(1,100)
 string_pool = string.ascii_lowercase # 소문자
 string_pool = string.ascii_uppercase # 대문자
+# function for a in range(n)  구문 암기 
 
-print("".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3)))
+print("".join(choice(string.ascii_uppercase + string.digits) for _ in range(3)))
