a66b0095938388c056d813d95c3b82a77b10359e
haeungray <haeungray@gmail.com>
2020-05-25 23:50:13 +0900

    class define

--- tkintertest.py
+++ tkintertest.py
@@ -7,3 +7,14 @@ from urllib import urlretrieve
 from urlparse import urlparse, urljoin
 from formatter import DumbWriter, AbstractFormatter
 from cStringIO import StringIO
+
+
+class Retriever:
+
+    def __init__(self, url):
+        self.url = url
+        self.file = self.filename(url)
+    
+    def filename(self url, deffile = "index.htm"):
+        # url path parsing
+        parsedurl = urlparse(url, "http:", 0)
